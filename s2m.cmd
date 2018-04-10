f5084a22268df481fcff913f3fe3112a80f21a15
SHIE, Li-Yi <lyshie@mail.nsysu.edu.tw>
2018-04-10 13:49:44 +0800

    Update path

--- s2m.cmd
+++ s2m.cmd
@@ -1,1 +1,2 @@
+"%localappdata%\Programs\Python\Python36-32\Scripts\s2m.exe" -l tws -s "%ProgramFiles(x86)%\Scratch 2\Scratch 2.exe"
 "%localappdata%\Programs\Python\Python36-32\Scripts\s2m.exe" -l tws -s "%ProgramFiles%\Scratch 2\Scratch 2.exe"
